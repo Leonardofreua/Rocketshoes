1af4269096610b3b9ad3d560871ceef6ec2d641a
leonardo.freua <leonardo.freua@cresolconfederacao.com.br>
2020-04-08 19:30:35 -0300

    Stylized the Home screen.

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -6,7 +6,7 @@ export const Container = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin: 50px 0;
+  /* margin: 50px 0; */
 `;
 
 export const Cart = styled(Link)`
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,112 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Productlist } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <Productlist>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-couro-nike-court-royale-masculino/26/D12-1936-026/D12-1936-026_zoom2.jpg?ts=1585887454&ims=326x"
+          alt="Shoes"
+        />
+        <strong>Shoes</strong>
+        <span>R$ 129.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>Add to Cart</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-couro-nike-court-royale-masculino/26/D12-1936-026/D12-1936-026_zoom2.jpg?ts=1585887454&ims=326x"
+          alt="Shoes"
+        />
+        <strong>Shoes</strong>
+        <span>R$ 129.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>Add to Cart</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-couro-nike-court-royale-masculino/26/D12-1936-026/D12-1936-026_zoom2.jpg?ts=1585887454&ims=326x"
+          alt="Shoes"
+        />
+        <strong>Shoes</strong>
+        <span>R$ 129.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>Add to Cart</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-couro-nike-court-royale-masculino/26/D12-1936-026/D12-1936-026_zoom2.jpg?ts=1585887454&ims=326x"
+          alt="Shoes"
+        />
+        <strong>Shoes</strong>
+        <span>R$ 129.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>Add to Cart</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-couro-nike-court-royale-masculino/26/D12-1936-026/D12-1936-026_zoom2.jpg?ts=1585887454&ims=326x"
+          alt="Shoes"
+        />
+        <strong>Shoes</strong>
+        <span>R$ 129.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>Add to Cart</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-couro-nike-court-royale-masculino/26/D12-1936-026/D12-1936-026_zoom2.jpg?ts=1585887454&ims=326x"
+          alt="Shoes"
+        />
+        <strong>Shoes</strong>
+        <span>R$ 129.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>Add to Cart</span>
+        </button>
+      </li>
+    </Productlist>
+  );
 }
