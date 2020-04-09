4c07931d31eafbbd1cc0d182d42a2a499ddb908b
leonardo.freua <leonardo.freua@cresolconfederacao.com.br>
2020-04-09 14:23:16 -0300

    Stylized the Cart screen.

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,68 @@
 import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <h1>Cart</h1>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUCT</th>
+            <th>QUANTITY</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-couro-nike-court-royale-masculino/26/D12-1936-026/D12-1936-026_zoom2.jpg?ts=1585887454&ims=326x"
+                alt="Shoes"
+              />
+            </td>
+            <td>
+              <strong>Descrição do tênis</strong>
+              <span>$ 129,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#28a745" />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#28a745" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>$ 258,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#28a745" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Check out</button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>$ 1920,28</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
